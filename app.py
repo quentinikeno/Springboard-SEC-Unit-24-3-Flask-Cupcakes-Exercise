6b0d0fcdc064e45737749baca0cd17a1e74aa67f
Quentin Ikeno <ikenoq@gmail.com>
2022-03-16 17:28:11 -1000

    Create post route for new cupcakes

--- app.py
+++ app.py
@@ -12,14 +12,30 @@ connect_db(app)
 
 @app.route("/api/cupcakes")
 def list_all_cupcakes():
-    """Return JSON of all cupcakes."""
+    """Return JSON of all cupcakes. {cupcakes: [{id, flavor, size, rating, image}, ...]}."""
     cupcakes = Cupcake.query.all()
     serialized = [cupcake.serialize() for cupcake in cupcakes]
     return jsonify(cupcakes=serialized)
 
 @app.route("/api/cupcakes/<int:id>")
 def list_single_cupcake(id):
-    """Return JSON of a single cupcake."""
+    """Return JSON of a single cupcake. {cupcake: {id, flavor, size, rating, image}}."""
     cupcake = Cupcake.query.get_or_404(id)
     serialized = cupcake.serialize()
     return jsonify(cupcake=serialized)
+
+@app.route("/api/cupcakes", methods=["POST"])
+def create_cupcake():
+    """Creates a new cupcake and returns JSON for new cupcake. {cupcakes: [{id, flavor, size, rating, image}, ...]}."""
+    flavor = request.json["flavor"]
+    size = request.json["size"]
+    rating = request.json["rating"]
+    image = request.json["image"]
+
+    new_cupcake = Cupcake(flavor=flavor, size=size, rating=rating, image=image)
+    db.session.add(new_cupcake)
+    db.session.commit()
+    
+    serialized = new_cupcake.serialize()
+    json = jsonify(cupcake=serialized)
+    return (json, 201)
